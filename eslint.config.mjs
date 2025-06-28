7577f34606618415a3837489eb8e0fbfbdc765be
stivenkno <stivencano931@gmail.com>
2025-06-28 01:38:04 -0500

    szss2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Aquí defines reglas personalizadas
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
